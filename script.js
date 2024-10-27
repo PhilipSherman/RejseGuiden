23cf1c226450a7f0a8287f47fc82887e151ca4a3
PhilipSherman <philipshermanlarsen@gmail.com>
2024-10-27 17:05:51 +0100

    start med js

---
+++ script.js
@@ -0,0 +1,10 @@
+const toj = { UndertojOgSokker: 0, tshirts: 0, langebukser: 0, shortNederdele: 0, Badetoj: 0, Jakke: 0, Sko: 0, Regnjakke: 0, Accesories: 0 }; //opretter en variabel for hver tøjstykke
+
+let Dageafsted = 20;
+
+if (Dageafsted == 20) {
+    toj.tshirts = 20;
+}
+
+console.log(toj)
+
