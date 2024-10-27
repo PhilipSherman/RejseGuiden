87dc80ed213afe20d4a3a71b43673b36ed76f489
PhilipSherman <philipshermanlarsen@gmail.com>
2024-10-27 22:04:21 +0100

    d

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const toj = { UndertojOgSokker: 0, tshirts: 0, langebukser: 0, shortNederdele: 0, Badetoj: 0, Jakke: 0, Sko: 0, Regnjakke: 0, Accesories: 0 }; //opretter en variabel for hver tøjstykke
+let toj = { UndertojOgSokker: 0, tshirts: 0, langebukser: 0, shortNederdele: 0, Badetoj: 0, Jakke: 0, Sko: 0, Regnjakke: 0, Accesories: 0 }; //opretter en variabel for hver tøjstykk
 
-let Dageafsted = 20;
-
-if (Dageafsted == 20) {
-    toj.tshirts = 20;
+function Dageafsted() {
+    let inputfelt = document.getElementById("inputTal");
+    document.getElementById("tshirts").innerHTML = ("T-Shirts: ") + Math.ceil(inputfelt.value * 1.2)
+    document.getElementById("undertoj").innerHTML = ("Undertøj: ") + Math.ceil(inputfelt.value * 1.5)
+    document.getElementById("langebukser").innerHTML = valgtvordi
 }
 
-console.log(toj)
 
