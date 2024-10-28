5d7775ba3a4d751cb9301136ab18f1dd9b3eee9c
PhilipSherman <philipshermanlarsen@gmail.com>
2024-10-28 23:56:54 +0100

    d

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-let toj = { UndertojOgSokker: 0, tshirts: 0, langebukser: 0, shortNederdele: 0, Badetoj: 0, Jakke: 0, Sko: 0, Regnjakke: 0, Accesories: 0 }; //opretter en variabel for hver tøjstykk
-
-function Dageafsted() {
-    let inputfelt = document.getElementById("inputTal");
-    document.getElementById("tshirts").innerHTML = ("T-Shirts: ") + Math.ceil(inputfelt.value * 1.2)
-    document.getElementById("undertoj").innerHTML = ("Undertøj: ") + Math.ceil(inputfelt.value * 1.5)
-    document.getElementById("langebukser").innerHTML = valgtvordi
+function LavListen() {
+    let dage = document.getElementById("inputTal").value;
+    document.getElementById("1").innerHTML = "Undertøj: " + Math.ceil(dage * 1.1) + " stk";
+    document.getElementById("2").innerHTML = "Bukser / Nederdele: " + Math.ceil(dage * 0.8) + " stk";
+    document.getElementById("3").innerHTML = "Sko: " + Math.ceil(dage * 0.1) + " stk";
+    document.getElementById("4").innerHTML = "Toiletsager";
+    document.getElementById("5").innerHTML = "Jakke (Hvis nødvendigt): " + Math.ceil(dage * 0.1) + " stk";
+    document.getElementById("6").innerHTML = "Regnjakke: " + Math.ceil(dage * 0.1) + " stk";
+    document.getElementById("7").innerHTML = "Accesories: Solbriller, bælte osv.";
+    document.getElementById("8").innerHTML = "Badetøj (Hvis nødvendigt): " + Math.ceil(dage * 0.2) + " stk";
+    document.getElementById("9").innerHTML = "Sokker: " + Math.ceil(dage * 1.1) + " stk"
+    document.getElementById("10").innerHTML = "T-Shirts / Toppe: " + Math.ceil(dage * 1.1) + " stk"
+    document.getElementById("11").innerHTML = "Tykke trøjer (Hvis nødvendigt): " + Math.ceil(dage * 0.5) + " stk"
 }
-
-
